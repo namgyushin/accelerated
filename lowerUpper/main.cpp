1cb46afd8c8d3f9e8b16bfce157f241d8a530ce2
namgyushin <namgyushin@naver.com>
2024-09-12 16:53:30 +0900

    .

---
+++ lowerUpper/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool isUpper(string s)
+{
+    bool result = false;
+
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+
+    return result;
+}
+
+int main()
+{
+    vector<string> lower, upper;
+
+    string.s;
+    while (cin >> s)
+    {
+        if (isUpper(s))
+        {
+            upper.push_back(s);
+        }
+        else
+        {
+            lower.push_back(s);
+        }
+        
+    }
+
+
+
+    return 0;
+}
